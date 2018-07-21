b703c2e246aeda557a6a0c9c919caf442a3cbfdb
Nicolay Poymanov <n.poymanov@gmail.com>
2018-07-21 11:26:32 +0300

    Extract price from data

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.century21.com/real-estate/rock-springs-wy/LCWYROCKSPRINGS/';
+url = 'https://www.pythonhow.com/real-estate/rock-springs-wy/LCWYROCKSPRINGS/';
 
 request = requests.get(url)
 content = request.content
 
 data = BeautifulSoup(content, 'html.parser')
+
+all = data.find_all('div', {'class': 'propertyRow'})
+
+for item in all:
+    price = item.find('h4').text.strip()
+    print(price)
